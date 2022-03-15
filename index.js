8ed7961ac34b2e893214ae5d979ab8a6b87dfa11
Liam Smith <liam.s.smith@gmail.com>
2022-03-15 03:29:50 +0000

    create callback waterfall in iss.js and call it in index

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-const { fetchMyIP, fetchCoordsByIp } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss')
 
-// fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes
-
-fetchMyIP((error, IP) => {
-  if (error) {
-    console.log("It didn't work: ", error);
-    return;
-
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log("It worked! Return IP: ", IP);
+};
 
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
 
+//nextISSTimesForMyLocation();
+////////////////////////////////////////////////
 
+// const { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes } = require('./iss');
+// // fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes
 
-fetchCoordsByIp('6.11.178.134', (error, geoCoordinates) => {
-  if (error) {
-    console.log("Coordinates unavailable: ", error);
-    return;
-  }
-  console.log("It worked! Coordinates at IP are: ", geoCoordinates);
-});
+// call function to fetch IP and parse
+// fetchMyIP((error, IP) => {
+//   if (error) {
+//     // Pass error back to callback
+//     console.log("It didn't work: ", error);
+//     return;
 
+//   }
+//   console.log("It worked! Return IP: ", IP);
 
+// });
 
+// // call function to fetch geoCoordinates by IP address
+// fetchCoordsByIp('6.11.178.134', (error, geoCoordinates) => {
+//   if (error) {
+//     console.log("Coordinates unavailable: ", error);
+//     return;
+//   }
+//   console.log("It worked! Coordinates at IP are: ", geoCoordinates);
+// });
 
-
-
-
+// // call function to fetch ISS flyover times at geolocation
+// fetchISSFlyOverTimes({ latitude: 37.751, longitude: -97.822 }, (error, flyOverTimes) => {
+//   if (error) {
+//     console.log("Flyover Times unavailable: ", error);
+//     return;
+//   }
+//   console.log("It worked! Flyover Times are:", flyOverTimes);
+// });
